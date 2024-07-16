26b5ba60eecb371e111e35e7173118e4716d7616
chogene <168207891+chogene@users.noreply.github.com>
2024-07-17 01:10:33 +0900

    Add files via upload

---
+++ myPythonProject/comConditional.py
@@ -0,0 +1,17 @@
+myNumber = int(input('Enter a Number: '))
+
+if myNumber < 0 and myNumber%2 == 0:
+    print('Your number is negative even')
+if myNumber < 0 and myNumber % 2 == 1:
+    print('Your number is negative odd')
+if myNumber > 0 and myNumber % 2 == 0:
+    print('Your number is positive even')
+if myNumber > 0 and myNumber % 2 == 1:
+    print('Your number is positive odd')
+if myNumber ==0:
+    print('Your number is zero')
+
+
+# input number is it between 5,10 including
+# input number your number is positive even odd positive even negative
+# zero is just zero
---
+++ myPythonProject/evenCheck.py
@@ -0,0 +1,9 @@
+x = int(input('Please input a number '))
+
+z = x%2
+
+if (z == 0):
+    print('The number is even')
+if z == 1:
+    print('The number is odd')
+
