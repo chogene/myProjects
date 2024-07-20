6ee83f31c94d95c627f23cec5d22a0776705f8f1
chogene <168207891+chogene@users.noreply.github.com>
2024-07-20 12:30:45 +0900

    Create functionHW.py

---
+++ myPythonProject/functionHW.py
@@ -0,0 +1,30 @@
+# write functions
+# input grades
+# print grades
+# avg grades
+# high/low grades
+x = []
+
+def inputGrades(y):
+    for i in range(0, y, 1):
+        text = 'Enter grade #', y
+        grade = float(input('Enter grade: '))
+        x.append(grade)
+    return x
+
+def avgGrades():
+    sum
+# def avgGrades(numGrades):
+
+#  highLow():
+
+numGrades = int(input('Enter number of grades: '))
+z = inputGrades(numGrades)
+
+
+# y = numGrades()
+# x = avgGrades()
+# z = highLow()
+
+
+print(z)
